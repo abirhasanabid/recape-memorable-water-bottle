145040f4ea90a86f0e6d90ba583715f25d64e2a2
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-11-18 15:45:57 +0600

    show selected bottle name in the other component

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,13 +1,14 @@
 import './bottle.css'
 import PropTypes from 'prop-types';
 
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleByNowBtn }) => {
     const { name, img, price } = bottle;
     return (
         <div className="bottle">
             <h3>Bottle : {name}</h3>
             <img src={img} alt="" />
             <p>Price : {price}</p>
+            <button onClick={()=>handleByNowBtn(bottle)}>ByeNow</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,24 +4,38 @@ import Bottle from "../Bottle/Bottle";
 
 const Bottles = () => {
 
-    const [bottles,setBottles]=useState([]);
+    const [bottles, setBottles] = useState([]);
+    // show btl name state 
+    const [bottls, setBottle] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
 
         fetch('./bottlesData.json')
-        .then(res => res.json())
-        .then(data => setBottles(data))
+            .then(res => res.json())
+            .then(data => setBottles(data))
 
-    },[])
+    }, []);
+
+    // ByeNow btn
+    const handleByNowBtn = bottle => {
+        const newBottleData = [...bottls,bottle];
+        setBottle(newBottleData)
+    }
 
     return (
         <div>
-            <h2>Bottles : {bottles.length}</h2>  
+            <h2>Bottles : {bottles.length}</h2>
 
-            <div style={{display:'grid',gridTemplateColumns:'repeat(3,1fr)'}}>
-            {
-                bottles.map(bottle=> <Bottle key={bottle.id} bottle={bottle}></Bottle>)
-            }
+            <div className="">
+                {
+                    bottls.map(bottl => <p key={bottl.id}>Selected Bottle Name : {bottl?.name}</p>)
+                }
+                
+            </div>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3,1fr)' }}>
+                {
+                    bottles.map(bottle => <Bottle handleByNowBtn={handleByNowBtn} key={bottle.id} bottle={bottle}></Bottle>)
+                }
             </div>
 
         </div>
